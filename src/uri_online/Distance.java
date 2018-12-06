f76141546e4470be40bc75491e1e108db809fd38
mahbubhmamun <hussainmahbub88@gmail.com>
2018-12-06 18:20:34 -0500

    Edited

--- src/uri_online/Distance.java
+++ src/uri_online/Distance.java
@@ -6,13 +6,10 @@ public class Distance {
 
 	static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int d = sc.nextInt();
-		int result = d*2;
-//		System.out.p
-		//int t = (int) (d / ((90 / 60.0) - (60 / 60.0)));
-		//System.out.printf("%d minutos\n", t);
-		System.out.printf("%d minutos\n", result);
+		int d;
+		d = sc.nextInt();
+		int t = (int) (d / ((90 / 60.0) - (60 / 60.0)));
+		System.out.printf("%d minutos\n", t);
 	}
 
 }
